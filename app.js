930444464eecd62738e74de0ba92afe4ca6df503
Mikey Mann <mikey.mannx3@gmail.com>
2020-11-22 12:30:16 -0800

    added pug partials with includes and extends

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ const express = require('express');
 
 const app = express();
 
-app.set('view engine','pug');
+
+app.set('view engine', 'pug');
 
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/hello', (req, res) => {
-    res.send("<h1>Hello, my name is Mikey :)</h1>");
+app.get('/cards', (req, res) => {
+    res.render('card', { prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is!" });
 });
+
+
 
 app.listen(3000, () => {
     console.log('The application is running on localhost:3000!')
