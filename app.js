12df36747a2384620c657f6f6eeb351aa40758ce
mikey mann <mikey.mannx3@gmail.com>
2020-11-23 11:04:49 -0800

    added body-parser

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.set('view engine', 'pug');
 
+//GET ROUTES
 
 app.get('/', (req, res) => {
     res.render('index');
@@ -14,6 +15,16 @@ app.get('/cards', (req, res) => {
     res.render('card', { prompt: "Who is buried in Grant's tomb?", hint: "Think about whose tomb it is!" });
 });
 
+app.get('/hello', (req, res) => {
+    res.render('hello');
+});
+
+// POST ROUTES
+
+app.post('/hello', (req,res) => {
+    console.dir(req.body);
+    res.render('hello');
+});
 
 
 app.listen(3000, () => {
