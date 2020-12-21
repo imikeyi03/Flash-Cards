8fd59e1949442ce352ea1691176bc2d5d7189c43
Mikey Mann <mikey.mannx3@gmail.com>
2020-12-21 08:39:34 -0800

    finished flashcard app

---
+++ public/js/app.js
@@ -0,0 +1,9 @@
+// Only show the hint when the user presses a button to view it
+
+// Hide the hint.
+
+// Create and append a button element to the DOM.
+
+// Add an event listener to the button to unhide the hint when the button is clicked.
+
+// Also, make sure the code only runs on the question side of the flashcard.
